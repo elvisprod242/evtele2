6c425b604e41f66f2f50c1755ee22c187bbc9161
elvisprod242 <elvisprod242@gmail.com>
2026-03-11 23:00:08 +0100

    Update TV stream URL in LiveTvPage component

--- src/app/live-tv/page.tsx
+++ src/app/live-tv/page.tsx
@@ -19,7 +19,7 @@ export default function LiveTvPage({}: {}) {
     const [views, setViews] = useState(10000);
     const [likes, setLikes] = useState(1000);
     const [isClient, setIsClient] = useState(false);
-    const [tvStreamUrl, setTvStreamUrl] = useState('https://c.streamhoster.com/link/hls/Wns3eE/i3hogHZs0zc/mtRZn0sX3Up_5/playlist.m3u8');
+    const [tvStreamUrl, setTvStreamUrl] = useState('https://live20.bozztv.com/akamaissh101/ssh101/evtele2xrdc/playlist.m3u8');
     
     const firestore = useFirestore();
 
